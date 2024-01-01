f79d177e187e99b5319c6121775804f96e766f90
Shahrear Mahmud <aodriteosrk10252@gmail.com>
2024-01-01 11:39:17 +0600

    Setup

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Move',
+  description: 'The connected workspace where better ,faster work happens',
+  icons: {
+    icon:[
+      {
+        media:"(prefers-color-scheme:light)",
+        url:"/logo.png",
+        href:"/logo.png"
+      },
+      {
+        media:"(prefers-color-scheme:dark)",
+        url:"/logo.png",
+        href:"/logo.png"
+      }
+    ]
+  }  
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
