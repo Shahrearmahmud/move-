6f81b3b7814d9fd05426576aba59374e33d35b70
Shahrear Mahmud <aodriteosrk10252@gmail.com>
2024-01-05 21:22:39 +0600

    10.editAndImplementDocumentsTitle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,53 +1,54 @@
-import {Toaster} from "sonner"
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { ThemeProvider } from '@/components/providers/theme-provider'
-import { ConvexClientProvider } from '@/components/providers/convex.provider'
+import { Toaster } from "sonner";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex.provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Move',
-  description: 'The connected workspace where better ,faster work happens',
+  title: "Move",
+  description: "The connected workspace where better ,faster work happens",
   icons: {
-    icon:[
+    icon: [
       {
-        media:"(prefers-color-scheme:light)",
-        url:"/logo.png",
-        href:"/logo.png"
+        media: "(prefers-color-scheme:light)",
+        url: "/logo.png",
+        href: "/logo.png",
       },
       {
-        media:"(prefers-color-scheme:dark)",
-        url:"/logo-dark.png",
-        href:"/logo-dark.png"
-      }
-    ]
-  }  
-}
+        media: "(prefers-color-scheme:dark)",
+        url: "/logo-dark.png",
+        href: "/logo-dark.png",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-      <ConvexClientProvider>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-        storageKey="move-theme"
-      >
-        <Toaster position="bottom-center" />
-      {children}
-      </ThemeProvider>
-      </ConvexClientProvider>
-        
-        </body>
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="move-theme"
+          >
+            <Toaster position="bottom-center" />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
+      </body>
     </html>
-  )
+  );
 }
