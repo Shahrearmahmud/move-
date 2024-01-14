8036b95fadf50b64136c2286eeadffe137e01e62
Shahrear Mahmud <aodriteosrk10252@gmail.com>
2024-01-14 21:08:42 +0600

    fix

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -14,7 +14,8 @@ export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Jotion</span>
+        Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
+        <span className="underline">Move</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
         Move is the connected workspace where <br />
@@ -42,5 +43,5 @@ export const Heading = () => {
         </SignInButton>
       )}
     </div>
-  )
-}
+  );
+};
